b73dedf25d2c4de68411820251b8599464405b18
Administrator <1097155803@qq.com>
2022-05-25 16:08:14 +0800

    init

---
+++ service-consumer02/src/main/java/com/wang/ServiceConsumer02Application.java
@@ -0,0 +1,13 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConsumer02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConsumer02Application.class, args);
+    }
+
+}
---
+++ service-consumer02/src/test/java/com/wang/ServiceConsumer02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConsumer02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
