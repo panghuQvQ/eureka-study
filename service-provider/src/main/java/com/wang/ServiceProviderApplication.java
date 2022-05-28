60d8130e57c597349da00248c70b5f388af1aad8
panghuQvQ <1097155803@QQ.com>
2022-05-28 15:33:08 +0800

    init

--- service-provider/src/main/java/com/wang/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/wang/ServiceProviderApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 // 开启EurekaClient注解，如果配置了Eureka客户端依赖，默认会开启该注解
 // @EnableEurekaClient
-// 开启熔断器注解
-//@EnableHystrix
 @SpringBootApplication
 public class ServiceProviderApplication {
 
