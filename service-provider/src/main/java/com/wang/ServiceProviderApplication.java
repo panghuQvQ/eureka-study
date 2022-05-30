2c18c5a3a41afdfc9e649708c13d336a9bb9ac50
panghuQvQ <1097155803@QQ.com>
2022-05-30 10:27:53 +0800

    init

--- service-provider/src/main/java/com/wang/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/wang/ServiceProviderApplication.java
@@ -2,9 +2,6 @@ package com.wang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 // 开启EurekaClient注解，如果配置了Eureka客户端依赖，默认会开启该注解
 // @EnableEurekaClient
